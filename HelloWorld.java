8983809803cd65dcbce2801c7425b18d74e98428
saisri09 <saisri.malempati@gmail.com>
2017-06-07 14:32:55 +0530

    commited

--- HelloWorld.java
+++ HelloWorld.java
@@ -22,6 +22,7 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 		System.out.println("Hello, Hyderabad");
+System.out.println("Hello,welocome to india");
     }
 
 }
